d72668bc37f1e3ca34f9eadacc0b5c12e04899c7
vfreire85 <victorfreire85@gmail.com>
2024-08-06 09:07:07 -0300

    Working User and REST entities

---
+++ src/main/java/com/victorfreire/springmongo1/Springmongo1Application.java
@@ -0,0 +1,13 @@
+package com.victorfreire.springmongo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmongo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmongo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorfreire/springmongo1/Springmongo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorfreire.springmongo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmongo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
