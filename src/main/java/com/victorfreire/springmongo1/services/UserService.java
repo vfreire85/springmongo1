96aa71a7337e5a01a4699225600f7f2f56ee3e91
vfreire85 <victorfreire85@gmail.com>
2024-08-09 14:08:46 -0300

    User insertion with POST

--- src/main/java/com/victorfreire/springmongo1/services/UserService.java
+++ src/main/java/com/victorfreire/springmongo1/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victorfreire.springmongo1.domain.User;
+import com.victorfreire.springmongo1.dto.UserDTO;
 import com.victorfreire.springmongo1.repositories.UserRepository;
 import com.victorfreire.springmongo1.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional <User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
